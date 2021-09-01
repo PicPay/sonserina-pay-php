f9282e759811848a249d6e18cd9a3fce82da5fdc
wellington_alencar <wellington.alencar@pravaler.com.br>
2021-09-01 16:20:26 -0300

    feat: :hammer: Implement FrauCheckerFactory
    Implement FrauCheckerFactory

--- src/App/Domain/Services/FraudChecker.php
+++ src/App/Domain/Services/FraudChecker.php
@@ -5,11 +5,33 @@ declare(strict_types=1);
 namespace App\Domain\Services;
 
 use App\Domain\Entities\Transaction;
+use App\Domain\Factorys\FraudCheckers\FraudCheckerFactory;
 
 class FraudChecker
 {
-    public function check(Transaction $transaction): bool
+    private $fraudCheckerIntegration;
+    private $sequenceClient = [1, 2];
+
+    public function check(Transaction $transaction, array $sequenceClient): bool
     {
-        return false;
+        return $this->checkClientConnect();
+    }
+
+    private function checkClientConnect()
+    {
+        $client = FraudCheckerFactory::getFraudCheckerOne();
+        $this->getFraudChecker($client);
+        
+        return $this->getFraudCheckerConnect();
+    }
+
+    private function getFraudChecker($client)
+    {
+        $this->fraudCheckerIntegration = FraudCheckerFactory::getFraudCheckerIntegration($client);
+    }
+
+    private function getFraudCheckerConnect()
+    {
+        $this->fraudCheckerIntegration->connect();
     }
 }
