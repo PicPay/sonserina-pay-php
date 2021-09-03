778fe846f8e2e91dd9302b2d643d3450249e8bc5
wellington_alencar <wellington.alencar@pravaler.com.br>
2021-09-03 17:42:48 -0300

    feat: :zap: Create new method transactionTaxValues to Calculate Tax
    Create new method transactionTaxValues to Calculate Tax

--- src/App/Domain/Services/TaxCalculator.php
+++ src/App/Domain/Services/TaxCalculator.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Domain\Services;
 
 use App\Domain\Contracts\TaxManagerClientInterface;
+use App\Domain\Entities\Transaction;
 
 class TaxCalculator
 {
@@ -38,12 +39,60 @@ class TaxCalculator
     }
 
     /**
+     * @param Transaction $transaction
+     * @return array
+     */
+    public function transactionTaxValues(Transaction $transaction): array
+    {
+        $initialAmount = $transaction->getInitialAmount();
+        $sellerTax = $transaction->getSellerTax();
+
+        $valueTotalWithTax = $this->calculate($initialAmount, $sellerTax);
+        $slytherinPayTax = $this->calculateSlytherinPayTax($initialAmount, $sellerTax, $valueTotalWithTax);
+        $totalTax = $this->calculateTotalTax($sellerTax, $slytherinPayTax);
+
+        $transactionTaxValues = [
+            'valueTotalWithTax' => $valueTotalWithTax,
+            'slytherinPayTax' => $slytherinPayTax,
+            'totalTax' => $totalTax,
+        ];
+
+        return $transactionTaxValues;
+    }
+
+    /**
      * @param float $amount
      * @param float $tax
      * @return float
      */
-    public function calculate(float $amount, float $tax): float
+    private function calculate(float $amount, float $tax): float
     {
         return $amount * $this->getRealTaxValue($tax);
+    }
+
+    /**
+     * @param float $initialAmount
+     * @param float $sellerTax
+     * @param float $totalValueWithTax
+     * @return float
+     */
+    private function calculateSlytherinPayTax(float $initialAmount, float $sellerTax, float $totalValueWithTax): float
+    {
+        $slytherinPayTax = abs(($sellerTax + $initialAmount) - $totalValueWithTax);
+        
+        return $slytherinPayTax;
+    }
+
+    /**
+     * 
+     * @param float $sellerTax
+     * @param float $slytherinPayTax
+     * @return float
+     */
+    private function calculateTotalTax(float $sellerTax, float $slytherinPayTax): float
+    {
+        $totalTax = abs($sellerTax + $slytherinPayTax);
+
+        return $totalTax;
     }
 }
