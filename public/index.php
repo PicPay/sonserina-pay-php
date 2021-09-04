64c0dfd626574a078ce4d787e2b7ce133d737948
wellington_alencar <wellington.alencar@pravaler.com.br>
2021-09-04 15:25:41 -0300

    refactor: :zap: Remove display erros
    Remove display erros

--- public/index.php
+++ public/index.php
@@ -2,10 +2,6 @@
 
 require __DIR__.'/../vendor/autoload.php';
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 use \App\Domain\Services\Transactions\TransactionExecutator;
 
 $simulateAuthorized = [
